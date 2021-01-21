3f839769a959930b36d7cb9e40aac1b252e0ec4b
Mateo Ferreira <matteo948@icloud.com>
2021-01-21 17:09:12 -0500

    prompt and QA template

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const inquirer = require('inquirer')
 const { type } = require('os')
-const fs  = require
+const fs  = require('fs')
 inquirer.prompt([
+{
+    type: 'input',
+    name: 'mame',
+    messege: "question",
 
-    type:'input'
-    name: 'W'
 
+},
+{
+    type:'input',
+    name: 'name',
+    messege:'question',
+
+},
+{
+    type:'input',
+    name: 'name',
+    messege:'question',
+
+},
+{
+    type:'input',
+    name: 'name',
+    messege:'question',
+
+}
 
 ])
